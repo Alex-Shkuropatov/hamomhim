5067bc4e485d274891c46b1c265335feef316153
handofblood <vladislav.manoly@gmail.com>
2019-07-17 13:20:50 +0300

    Add Stats component, About page- add adaptive

---
+++ assets/js/components/Stats.vue
@@ -0,0 +1,92 @@
+<template>
+
+      <div class="stats">
+        <div class="stats_element">
+          <img src="/static/images/aboutUs/worker.png" alt="worker">
+          <span class="stats_numbers">307</span>
+          <span class="stats_txt">קבלנים</span>
+        </div>
+        <hr class="line">
+        <div class="stats_element">
+          <img src="/static/images/aboutUs/case.png" alt="case" class="caseImg">
+          <span class="stats_numbers">1869</span>
+          <span class="stats_txt">הצעות שעברו באתר</span>
+        </div>
+        <hr class="line">
+        <div class="stats_element">
+          <img src="/static/images/aboutUs/community.png" alt="community">
+          <span class="stats_numbers">1357</span>
+          <span class="stats_txt">משתמשים</span>
+        </div>
+      </div>
+
+</template>
+
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss" scoped>
+  .stats {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 0 25px 0 25px;
+    @media screen and (max-width: 900px) {
+      flex-direction: column;
+    }
+    .stats_element {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      flex-wrap: wrap;
+
+      .stats_numbers {
+        font-family: Assistant;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 72px;
+        line-height: 65px;
+        letter-spacing: -0.02em;
+        background: linear-gradient(90deg, #2871D7 0%, #3269B6 100%);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        margin-top: 18px;
+      }
+
+      .stats_txt {
+        text-align: center;
+        margin-top: 12px;
+        font-family: Assistant;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 48px;
+        line-height: 65px;
+        letter-spacing: -0.02em;
+        color: #828282;
+      }
+      .caseImg{
+        @media screen and (max-width:1110px ){
+          margin-top: 61px;
+        }
+      }
+    }
+
+    .line {
+      width: 195px;
+      height: 0px;
+      border: 1px solid #BDBDBD;
+      transform: rotate(90deg);
+      @media screen and (max-width: 900px) {
+        margin: 20px 0 20px 0;
+        transform: rotate(0deg);
+      }
+      @media screen and (max-width: 1100px) {
+        margin: 20px 0 20px 0;
+        transform: rotate(0deg);
+      }
+    }
+  }
+
+</style>
--- assets/js/views/ContactUs.vue
+++ assets/js/views/ContactUs.vue
@@ -1,5 +1,26 @@
 <template>
-
+<div class="contact">
+<div class="formWrapper">
+  <form action="" id="contactForm">
+    <h2 class="title">צור קשר</h2>
+    <div class="textWrapper">
+      <div class="row">
+        <input type="text" placeholder="" >
+        <input type="text" placeholder="  מייל">
+      </div>
+      <div class="row">
+        <input type="text" placeholder="  איזור">
+        <input type="text" placeholder="  מספר נייד">
+      </div>
+      <textarea name="msg" id="" cols="30" placeholder="     הודעה חופשית" rows="10"></textarea>
+    </div>
+    <input type="button" value="  שליחת הודעה">
+  </form>
+</div>
+  <div class="mapW">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1881.2244956489853!2d-99.15884129732498!3d19.436199370379086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f8cdc19e3477%3A0xe98bef6360302321!2sUVM+Campus+San+Rafael!5e0!3m2!1sru!2sua!4v1563356851952!5m2!1sru!2sua" width="100%" height="529px" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </div>
+</div>
 </template>
 
 <script>
@@ -8,4 +29,79 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @mixin placeholder {
+    &::-webkit-input-placeholder {@content}
+    &:-moz-placeholder           {@content}
+    &::-moz-placeholder          {@content}
+    &:-ms-input-placeholder      {@content}
+  }
+
+.formWrapper{
+  background: url("/static/images/aboutUs/team.png");
+  background-position: left -87px ;
+  background-size: cover;
+  background-repeat: no-repeat;
+  height:  746px;
+  #contactForm{
+    padding: 15px 195px 43px 0;
+    .title{
+      margin: 0;
+      font-family: Assistant;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 48px;
+      line-height: 65px;
+      text-align: right;
+      color: #FFFFFF;
+      padding-top: 20px;
+    }
+    input[type="text"]{
+      margin-top: 19px;
+      background: #FFFFFF;
+      opacity: 0.5;
+      box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+      border-radius: 50px;
+      width: 490px;
+      height: 65px;
+      padding: 25px;
+
+    }
+    textarea{
+      margin-top: 19px;
+      background: #FFFFFF;
+      opacity: 0.5;
+      box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+      border-radius: 50px;
+      width: 1007px;
+      height: 280px;
+    }
+    input[type="button"]{
+      margin-top: 19px;
+      background: linear-gradient(90deg, #2871D7 0%, #3269B6 100%);
+      border-radius: 50px;
+      width: 355px;
+      height: 70px;
+      border: none;
+    }
+  }
+}
+.textWrapper{
+
+  .row{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+}
+  .mapW{
+    width: 100%;
+    height: 529px;
+
+  }
+button:active, button:focus {
+  outline: none;
+}
+button::-moz-focus-inner {
+  border: 0;
+}
 </style>
