5f506d88aeecc47f559f975ec322b7a43fcec3de
handofblood <vladislav.manoly@gmail.com>
2019-08-01 12:51:02 +0300

    Merge dev-main

--- static/js/app.js
+++ static/js/app.js
@@ -6603,6 +6603,32 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
@@ -6610,7 +6636,10 @@ __webpack_require__.r(__webpack_exports__);
       user: {
         title: 'שמתשמ םש',
         imageProfile: '/static/images/profile/defPicCutted.png',
-        position: 'האדר שייכל'
+        position: 'האדר שייכל',
+        valet: ' $235 ',
+        value: '$235',
+        valueText: 'לכייש רדאה:'
       }
     };
   },
@@ -9983,7 +10012,7 @@ exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loa
 
 
 // module
-exports.push([module.i, ".page-subheader[data-v-fc8cb6f0] {\n  position: relative;\n  height: 451px;\n  width: 100%;\n  display: flex;\n  -ms-align-items: center;\n  align-items: center;\n  flex-direction: column;\n  background-size: cover;\n  background-position: center;\n}\n.page-subheader .h-container[data-v-fc8cb6f0] {\n  width: unset;\n  display: flex;\n  flex-direction: column;\n  align-items: center;\n  text-align: center;\n}\n.page-subheader .h-container .userImage[data-v-fc8cb6f0] {\n  pointer-events: none;\n}\n.page-subheader .h-container .title[data-v-fc8cb6f0] {\n  pointer-events: none;\n  margin: 0;\n  margin-bottom: -20px;\n  font-family: Assistant;\n  font-style: normal;\n  font-weight: bold;\n  font-size: 36px;\n  line-height: 65px;\n  text-align: center;\n  color: #FFFFFF;\n}\n.page-subheader .h-container .position[data-v-fc8cb6f0] {\n  pointer-events: none;\n  font-family: Assistant;\n  font-style: normal;\n  font-weight: normal;\n  font-size: 24px;\n  line-height: 65px;\n  text-align: center;\n  color: #F2F2F2;\n}\n.nav[data-v-fc8cb6f0] {\n  position: absolute;\n  bottom: 0;\n  width: 100%;\n  background: linear-gradient(90deg, #2871D7 0%, #3269B6 100%);\n}\n.nav .nav-links[data-v-fc8cb6f0] {\n  margin: 0 auto;\n  margin-top: 10px;\n  margin-bottom: 10px;\n  width: 1000px;\n  display: flex;\n  flex-direction: row;\n  align-items: center;\n  list-style: none;\n}\n.nav .nav-links a li[data-v-fc8cb6f0] {\n  list-style-type: none;\n  font-family: Assistant;\n  font-style: normal;\n  font-weight: bold;\n  font-size: 24px;\n  line-height: 65px;\n  display: flex;\n  align-items: center;\n  text-align: center;\n  color: #FFFFFF;\n}\n.nav .nav-links a li svg[data-v-fc8cb6f0] {\n  margin-right: 12px;\n}\n.nav .nav-links a[data-v-fc8cb6f0]:hover {\n  text-decoration: underline;\n}\n.nav .nav-links hr[data-v-fc8cb6f0] {\n  width: 46px;\n  border: 1px solid #FFFFFF;\n  transform: rotate(90deg);\n}\n.userImage[data-v-fc8cb6f0] {\n  margin-top: 46px;\n  width: 200px;\n  height: 200px;\n  border-radius: 50%;\n}", ""]);
+exports.push([module.i, ".page-subhead[data-v-fc8cb6f0] {\n  position: relative;\n  height: 451px;\n  width: 100%;\n  display: flex;\n  flex-direction: row;\n  justify-content: flex-start;\n  background-size: cover;\n  background-position: center;\n}\n.page-subhead .head-container[data-v-fc8cb6f0] {\n  height: 317px;\n  margin-right: 358px;\n  width: unset;\n  display: flex;\n  flex-direction: row;\n  align-items: center;\n  text-align: center;\n}\n@media screen and (max-width: 1440px) {\n.page-subhead .head-container[data-v-fc8cb6f0] {\n    margin: 0 auto;\n}\n}\n.page-subhead .head-container .userImage[data-v-fc8cb6f0] {\n  pointer-events: none;\n}\n.page-subhead .head-container .title[data-v-fc8cb6f0] {\n  pointer-events: none;\n  margin: 0;\n  margin-bottom: -27px;\n  font-family: Assistant;\n  font-style: normal;\n  font-weight: bold;\n  font-size: 36px;\n  line-height: 65px;\n  text-align: right;\n  color: #FFFFFF;\n}\n.page-subhead .head-container .position[data-v-fc8cb6f0] {\n  pointer-events: none;\n  font-family: Assistant;\n  font-style: normal;\n  font-weight: normal;\n  font-size: 24px;\n  line-height: 65px;\n  text-align: right;\n  color: #F2F2F2;\n}\n.head-txt-container[data-v-fc8cb6f0] {\n  margin-right: 13px;\n}\n.head-txt-container hr[data-v-fc8cb6f0] {\n  margin-left: 15px;\n  width: 185px;\n  height: 0px;\n  color: white;\n  border: 1px solid #FFFFFF;\n  transform: rotate(-180deg);\n}\n.head-txt-container .about-info[data-v-fc8cb6f0] {\n  margin-bottom: -36px;\n}\n.head-txt-container .summary-info .sum[data-v-fc8cb6f0] {\n  font-family: Assistant;\n  font-style: normal;\n  font-weight: bold;\n  font-size: 24px;\n  line-height: 65px;\n  margin-top: -17px;\n  text-align: center;\n  display: flex;\n  align-items: center;\n  color: #F2F2F2;\n}\n.head-txt-container .summary-info .sum span[data-v-fc8cb6f0] {\n  font-weight: normal;\n}\n.head-txt-container .summary-info .sum svg[data-v-fc8cb6f0] {\n  margin-left: 10px;\n}\n.head-txt-container .summary-info .valet[data-v-fc8cb6f0] {\n  margin-top: -24px;\n}\n.nav[data-v-fc8cb6f0] {\n  position: absolute;\n  bottom: 0;\n  width: 100%;\n  background: linear-gradient(90deg, #2871D7 0%, #3269B6 100%);\n}\n.nav .nav-links[data-v-fc8cb6f0] {\n  margin: 0 auto;\n  margin-top: 10px;\n  margin-bottom: 10px;\n  width: 1000px;\n  display: flex;\n  flex-direction: row;\n  align-items: center;\n  list-style: none;\n}\n.nav .nav-links a li[data-v-fc8cb6f0] {\n  list-style-type: none;\n  font-family: Assistant;\n  font-style: normal;\n  font-weight: bold;\n  font-size: 24px;\n  line-height: 65px;\n  display: flex;\n  align-items: center;\n  text-align: center;\n  color: #FFFFFF;\n}\n.nav .nav-links a li svg[data-v-fc8cb6f0] {\n  margin-right: 12px;\n}\n.nav .nav-links a[data-v-fc8cb6f0]:hover {\n  text-decoration: underline;\n}\n.nav .nav-links hr[data-v-fc8cb6f0] {\n  width: 46px;\n  border: 1px solid #FFFFFF;\n  transform: rotate(90deg);\n}\n.userImage[data-v-fc8cb6f0] {\n  width: 200px;\n  height: 200px;\n  border-radius: 50%;\n}", ""]);
 
 // exports
 
@@ -23033,19 +23062,161 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", { staticClass: "page-subheader", style: _vm.styles }, [
-    _c("div", { staticClass: "h-container" }, [
+  return _c("div", { staticClass: "page-subhead", style: _vm.styles }, [
+    _c("div", { staticClass: "head-container" }, [
       _c("img", {
         staticClass: "userImage",
         attrs: { src: _vm.user.imageProfile, alt: "user-image" }
       }),
       _vm._v(" "),
-      _c("div", { staticClass: "th-heading title" }, [
-        _vm._v(_vm._s(_vm.user.title))
-      ]),
-      _vm._v(" "),
-      _c("div", { staticClass: "th-heading position" }, [
-        _vm._v(_vm._s(_vm.user.position))
+      _c("div", { staticClass: "head-txt-container" }, [
+        _c("div", { staticClass: "about-info" }, [
+          _c("div", { staticClass: "th-heading title" }, [
+            _vm._v(_vm._s(_vm.user.title))
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "th-heading position" }, [
+            _vm._v(_vm._s(_vm.user.position))
+          ])
+        ]),
+        _vm._v(" "),
+        _c("hr"),
+        _vm._v(" "),
+        _c("div", { staticClass: "summary-info" }, [
+          _c("div", { staticClass: "sum" }, [
+            _c(
+              "svg",
+              {
+                attrs: {
+                  width: "28",
+                  height: "25",
+                  viewBox: "0 0 28 25",
+                  fill: "none",
+                  xmlns: "http://www.w3.org/2000/svg"
+                }
+              },
+              [
+                _c("path", {
+                  attrs: {
+                    d:
+                      "M8.13511 14.4287C8.92198 14.4287 9.68256 14.3779 10.4023 14.2832V6.39439C9.68263 6.29985 8.92198 6.24902 8.13511 6.24902C3.64222 6.24902 0 7.90092 0 9.93872V10.7389C0 12.7766 3.64222 14.4287 8.13511 14.4287Z",
+                    fill: "white"
+                  }
+                }),
+                _vm._v(" "),
+                _c("path", {
+                  attrs: {
+                    d:
+                      "M8.13511 18.5474C8.92198 18.5474 9.68256 18.4965 10.4023 18.4021V16.0897C9.68242 16.1842 8.92219 16.2358 8.13511 16.2358C4.2133 16.2358 0.939977 14.977 0.171095 13.3018C0.0591233 13.5458 0 13.7984 0 14.0575V14.8577C0 16.8955 3.64222 18.5474 8.13511 18.5474Z",
+                    fill: "white"
+                  }
+                }),
+                _vm._v(" "),
+                _c("path", {
+                  attrs: {
+                    d:
+                      "M10.4023 20.3895V20.2098C9.68242 20.3043 8.92218 20.3559 8.13511 20.3559C4.2133 20.3559 0.939977 19.0971 0.171095 17.4219C0.0591233 17.6658 0 17.9185 0 18.1775V18.9777C0 21.0156 3.64222 22.6675 8.13511 22.6675C9.0336 22.6675 9.89779 22.6012 10.7059 22.4792C10.5056 22.0695 10.4023 21.6373 10.4023 21.1897V20.3895Z",
+                    fill: "white"
+                  }
+                }),
+                _vm._v(" "),
+                _c("path", {
+                  attrs: {
+                    d:
+                      "M19.8649 0C15.372 0 11.7298 1.6519 11.7298 3.6897V4.48989C11.7298 6.52769 15.372 8.17959 19.8649 8.17959C24.3577 8.17959 28 6.52769 28 4.48989V3.6897C28 1.6519 24.3577 0 19.8649 0Z",
+                    fill: "white"
+                  }
+                }),
+                _vm._v(" "),
+                _c("path", {
+                  attrs: {
+                    d:
+                      "M19.8649 9.98771C15.9431 9.98771 12.6698 8.72903 11.9009 7.05371C11.7889 7.29766 11.7298 7.55033 11.7298 7.80942V8.6096C11.7298 10.6474 15.372 12.2993 19.8649 12.2993C24.3577 12.2993 28 10.6474 28 8.6096V7.80942C28 7.55033 27.9409 7.29766 27.8289 7.05371C27.06 8.72903 23.7867 9.98771 19.8649 9.98771Z",
+                    fill: "white"
+                  }
+                }),
+                _vm._v(" "),
+                _c("path", {
+                  attrs: {
+                    d:
+                      "M19.8649 14.1059C15.9431 14.1059 12.6698 12.8471 11.9009 11.1719C11.7889 11.4158 11.7298 11.6685 11.7298 11.9276V12.7278C11.7298 14.7656 15.372 16.4175 19.8649 16.4175C24.3577 16.4175 28 14.7656 28 12.7278V11.9276C28 11.6685 27.9409 11.4158 27.8289 11.1719C27.06 12.8472 23.7867 14.1059 19.8649 14.1059Z",
+                    fill: "white"
+                  }
+                }),
+                _vm._v(" "),
+                _c("path", {
+                  attrs: {
+                    d:
+                      "M19.8649 18.4486C15.9431 18.4486 12.6698 17.1899 11.9009 15.5146C11.7889 15.7587 11.7298 16.0113 11.7298 16.2703V17.0705C11.7298 19.1083 15.372 20.7602 19.8649 20.7602C24.3577 20.7602 28 19.1083 28 17.0705V16.2703C28 16.0113 27.9409 15.7585 27.8289 15.5146C27.06 17.1898 23.7867 18.4486 19.8649 18.4486Z",
+                    fill: "white"
+                  }
+                }),
+                _vm._v(" "),
+                _c("path", {
+                  attrs: {
+                    d:
+                      "M19.8649 22.5678C15.9431 22.5678 12.6698 21.3091 11.9009 19.6339C11.7889 19.8779 11.7298 20.1306 11.7298 20.3895V21.1897C11.7298 23.2275 15.372 24.8794 19.8649 24.8794C24.3577 24.8794 28 23.2275 28 21.1897V20.3894C28 20.1304 27.9409 19.8777 27.8289 19.6338C27.06 21.309 23.7867 22.5678 19.8649 22.5678Z",
+                    fill: "white"
+                  }
+                })
+              ]
+            ),
+            _vm._v(" "),
+            _c("span", [_vm._v(_vm._s(_vm.user.valueText))]),
+            _vm._v(_vm._s(_vm.user.value) + "\n        ")
+          ]),
+          _vm._v(" "),
+          _c("div", { staticClass: "sum valet" }, [
+            _c(
+              "svg",
+              {
+                attrs: {
+                  width: "28",
+                  height: "27",
+                  viewBox: "0 0 28 27",
+                  fill: "none",
+                  xmlns: "http://www.w3.org/2000/svg"
+                }
+              },
+              [
+                _c("path", {
+                  attrs: {
+                    d:
+                      "M6.54308 5.11639L17.7054 2.06957L17.1142 0.874731C16.7275 0.0980891 15.7842 -0.22263 15.0075 0.164119L5.01495 5.11639H6.54308Z",
+                    fill: "white"
+                  }
+                }),
+                _vm._v(" "),
+                _c("path", {
+                  attrs: {
+                    d:
+                      "M21.1703 2.15723C21.0319 2.15723 20.8936 2.17609 20.7552 2.21382L18.1329 2.93072L10.1243 5.11601H19.2145H23.1763L22.6858 3.31747C22.4972 2.61629 21.862 2.15723 21.1703 2.15723Z",
+                    fill: "white"
+                  }
+                }),
+                _vm._v(" "),
+                _c("path", {
+                  attrs: {
+                    d:
+                      "M24.8148 6.21777H24.4564H23.969H23.4816H19.7619H6.09366H4.3014H2.79214H2.5123H1.57529C1.0785 6.21777 0.635149 6.44731 0.345873 6.8089C0.213812 6.97555 0.113195 7.16735 0.0565974 7.37802C0.0220101 7.51008 0 7.64843 0 7.78992V7.97858V9.77083V25.3068C0 26.1747 0.704323 26.879 1.57215 26.879H24.8117C25.6795 26.879 26.3838 26.1747 26.3838 25.3068V20.9205H17.0547C15.58 20.9205 14.382 19.7225 14.382 18.2479V16.8078V16.3204V15.833V14.7514C14.382 14.0282 14.6713 13.3711 15.1398 12.89C15.5549 12.4624 16.1114 12.1731 16.734 12.1008C16.8377 12.0882 16.9446 12.0819 17.0515 12.0819H25.0758H25.5632H26.0505H26.3838V7.78992C26.387 6.9221 25.6826 6.21777 24.8148 6.21777Z",
+                    fill: "white"
+                  }
+                }),
+                _vm._v(" "),
+                _c("path", {
+                  attrs: {
+                    d:
+                      "M27.4874 13.5837C27.3301 13.4391 27.1446 13.3291 26.9371 13.2567C26.7767 13.2033 26.607 13.1719 26.4277 13.1719H26.3869H26.3554H25.868H24.1104H17.0546C16.1867 13.1719 15.4824 13.8762 15.4824 14.744V15.5269V16.0143V16.5017V18.2436C15.4824 19.1114 16.1867 19.8158 17.0546 19.8158H26.3869H26.4277C26.607 19.8158 26.7768 19.7843 26.9371 19.7309C27.1446 19.6617 27.3301 19.5485 27.4874 19.4038C27.8018 19.1177 27.9999 18.7027 27.9999 18.2436V14.744C27.9999 14.2849 27.8018 13.8698 27.4874 13.5837ZM20.3372 16.8067C20.3372 17.2406 19.9851 17.5927 19.5511 17.5927H19.0292C18.5953 17.5927 18.2431 17.2406 18.2431 16.8067V16.2847C18.2431 16.0332 18.3595 15.8099 18.545 15.6684C18.6802 15.5646 18.8469 15.4986 19.0292 15.4986H19.1613H19.5511C19.9851 15.4986 20.3372 15.8508 20.3372 16.2847V16.8067Z",
+                    fill: "white"
+                  }
+                })
+              ]
+            ),
+            _vm._v(" "),
+            _c("span", [_vm._v(_vm._s(_vm.user.valueText))]),
+            _vm._v(" " + _vm._s(_vm.user.valet))
+          ])
+        ])
       ])
     ]),
     _vm._v(" "),
