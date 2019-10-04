06df14085e3275fb00d839a8d1e31a74d7c96462
a.shkuropatov <a.shkuropatov.workbox@gmail.com>
2019-10-04 17:19:18 +0300

    Contact form on main page

--- assets/js/components/modals/editWorkerProfile/ChangePassword.vue
+++ assets/js/components/modals/editWorkerProfile/ChangePassword.vue
@@ -1,32 +1,32 @@
 <template>
   <transition name="slide-fade">
     <modal v-if="$store.getters['modals/changePassword/isOpened']" @close="close">
-      <div class="content-wrapper">
-        <h2 class="title">Change password</h2>
+      <form class="content-wrapper">
+        <h2 class="title">שנה סיסמא</h2>
         <div class="popup-form-row">
           <div class="col1-2 inp-group">
-            <div class="label">סיסמה נוכחית</div>
-            <theme-input v-model="curPass" placeholder="סיסמה נוכחית" />
+            <div class="label">אימייל</div>
+            <theme-input :value="$store.getters['user/getField']('email')" disabled placeholder="אימייל" />
           </div>
           <div class="col1-2 inp-group">
-            <div class="label">אימייל</div>
-            <theme-input v-model="email" placeholder="אימייל" />
+            <div class="label">סיסמה נוכחית</div>
+            <theme-input v-model="curPass" type="password" required placeholder="סיסמה נוכחית" />
           </div>
         </div>
         <div class="popup-form-row">
           <div class="col1-2 inp-group">
             <div class="label">סיסמה חדשה</div>
-            <theme-input v-model="newPass" placeholder="סיסמה חדשה" />
+            <theme-input v-model="newPass" type="password" required minlength="6" placeholder="סיסמה חדשה" />
           </div>
           <div class="col1-2 inp-group">
             <div class="label">אשר סיסמה</div>
-            <theme-input v-model="confirmPass" placeholder="אשר סיסמה" />
+            <theme-input v-model="confirmPass" type="password" required minlength="6" placeholder="אשר סיסמה" />
           </div>
         </div>
         <div class="popup-form-row submit-btn-wrap">
-          <button class="th-btn th-btn-blue th-lg-btn">רומש</button>
+          <button class="th-btn th-btn-blue th-btn-lg" @click.prevent="changePassword">שמור</button>
         </div>
-      </div>
+      </form>
     </modal>
   </transition>
 </template>
@@ -36,20 +36,6 @@ import Modal from '../../common/Modal.vue'
 import ThemeInput from '../../common/ThemeInput.vue'
 
 export default {
-  data(){
-    return {
-
-    }
-  },
-  methods: {
-    close() {
-      this.$store.commit('modals/changePassword/close');
-    },
-  },
-  components: {
-    Modal,
-    ThemeInput,
-  },
   data() {
     return {
       newPass: '',
@@ -58,7 +44,35 @@ export default {
     }
   },
   methods: {
-
+    close() {
+      this.$store.commit('modals/changePassword/close');
+    },
+    changePassword(){
+      if(this.newPass === this.confirmPass){
+        let data = {
+          userId: this.$store.getters['user/getField']('id'),
+          oldPassword: this.curPass,
+          newPassword: this.newPass,
+        }
+        axios.post('/api/changePassword', data)
+          .then(response => {
+            if(response.data.success){
+              this.close();
+              alert('הסיסמא שונתה בהצלחה');//Password successfully changed
+            }
+            else{
+              alert(response.data.message);
+            }
+          });
+      }
+      else{
+        alert('סיסמאות לא תואמות');
+      }
+    }
+  },
+  components: {
+    Modal,
+    ThemeInput,
   },
   mounted(){
   },
@@ -71,10 +85,11 @@ export default {
 <style lang="scss" scoped>
 @import '~@/vars.scss';
 .modal .content-wrapper .title{
-  margin-bottom: 25px;
+  margin-bottom: ceil($scale1 * 25px);
 }
 .submit-btn-wrap{
   justify-content: center;
+  margin-top: ceil($scale1 * 25px);
 }
 
 .slide-fade-enter-active {
